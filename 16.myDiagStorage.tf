d8c2f4404b670cbf9bc2ce33ebbc1cafcc9ee0d2
junebuck <90170084+junebuck@users.noreply.github.com>
2021-09-29 19:32:31 +0900

    Create 16.myDiagStorage.tf

---
+++ 16.myDiagStorage.tf
@@ -0,0 +1,11 @@
+resource "azurerm_storage_account" "mystorageaccount" {
+    name                = "user27vm1diagestorage1"
+    resource_group_name = azurerm_resource_group.user27-rg.name
+    location = azurerm_resource_group.user27-rg.location
+    account_replication_type = "LRS"
+    account_tier = "Standard"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
