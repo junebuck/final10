20e6213d85f700897e463ea5510357ffa4f3aa34
junebuck <90170084+junebuck@users.noreply.github.com>
2021-09-30 13:25:13 +0900

    Update 10.mynsg.tf

--- 10.mynsg.tf
+++ 10.mynsg.tf
@@ -14,7 +14,18 @@ resource "azurerm_network_security_group" "mynsg" {
         source_address_prefix      = "58.224.211.202"    
         destination_address_prefix = "*"
     }
- security_rule {
+    security_rule {
+        name                       = "SSH-Jenkins"
+        priority                   = 1002
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "58.224.211.202"   #Jenkins IP 넣어야 함   
+        destination_address_prefix = "*"
+    }   
+    security_rule {
         name                       = "HTTP"
         priority                   = 2001
         direction                  = "Inbound"
