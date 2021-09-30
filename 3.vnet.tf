fa042fe033913bc5dd92f398d99a052f290958d8
junebuck <90170084+junebuck@users.noreply.github.com>
2021-09-30 15:07:47 +0900

    Update 3.vnet.tf

--- 3.vnet.tf
+++ 3.vnet.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "user27-net" {
     name = "user27-vnet"
-    address_space = ["40.0.0.0/16"]
+    address_space = ["27.0.0.0/16"]
     location = azurerm_resource_group.user27-rg.location
     resource_group_name = azurerm_resource_group.user27-rg.name
 }
